d02193009027d85c644e06bc944871c3a4808434
samPuriwat <sam.puriwat@gmail.com>
2018-11-19 16:35:04 +0700

    Lab 1 commit

--- src/lab1/intro.java
+++ src/lab1/intro.java
@@ -1,4 +1,21 @@
 package lab1;
 
 public class intro {
-}
+    //to do code here
+    public static void main(String[] args) {
+
+        System.out.println("puriwat lertkrai");
+        System.out.println("sam.puriwat@gmail.com");
+        System.out.println("MT");
+        System.out.println("RMUTSV");
+
+
+
+
+    }//main
+
+
+
+
+
+}//class
