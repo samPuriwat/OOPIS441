fec13d9ece66c5b6819eec98e9ea030cac7bbbfc
samPuriwat <sam.puriwat@gmail.com>
2018-12-17 15:15:09 +0700

    Lab 4: Method

---
+++ src/lab4/ex2.java
@@ -0,0 +1,14 @@
+package lab4;
+
+public class ex2 {
+
+    public static void main(String[] args) {
+        int x = summation(10, 10);
+    }//main
+
+    private static int summation(int i, int i1) {
+        return i+i1;
+    }
+
+
+}//class
