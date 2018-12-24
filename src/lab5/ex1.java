aa00357c0631cd38dc30c8e32e66fd4cc7884d6b
samPuriwat <sam.puriwat@gmail.com>
2018-12-24 14:29:32 +0700

    Lab 5: Sorting Data

--- src/lab5/ex1.java
+++ src/lab5/ex1.java
@@ -1,5 +1,6 @@
 package lab5;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class ex1 {
@@ -19,11 +20,38 @@ public class ex1 {
         //finding minimum integer in array
         findMin(myNum);
         //finding maximum integer in array
-
-
-
+        findMax(myNum);
+        //finding average value in array
+        findAverage(myNum);
+        //sorting array low -> high
+        sortArray(myNum);
 
     }//main
+
+    private static void sortArray(int[] myNum) {
+       Arrays.sort(myNum);
+       System.out.println("Sorting Data in array: ");
+       showData(myNum);
+
+
+    }
+
+    private static void findAverage(int[] myNum) {
+        double total = 0;
+        for (int val:myNum) {
+            total += val;
+        }
+        System.out.println("The average value is: "+(total/myNum.length));
+    }
+
+    private static void findMax(int[] myNum) {
+        int max = myNum[0];
+        for (int i = 0; i < myNum.length ; i++) {
+            if (max<myNum[i])
+                max = myNum[i];
+        }
+        System.out.println("The maximum integer is: "+max);
+    }
 
     private static void findMin(int[] myNum) {
         int min = myNum[0];
